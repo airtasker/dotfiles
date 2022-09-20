4ed42671d69f670dbf2dd0d4ce66f2cab7055e7e
Micah van der Kolk <micahvdk@gmail.com>
2022-09-20 16:17:57 +1000

    install autosuggestions and syntax highlighting

--- bootstrap.sh
+++ bootstrap.sh
@@ -44,6 +44,16 @@ else
     fi
 fi
 
+# Install zsh-autosuggestions
+if [[ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions ]]; then
+    git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
+fi
+
+# Install zsh-syntax-highlighting
+if [[ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting ]]; then
+    git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
+fi
+
 # Install powerlevel10k
 if [[ ! -d ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k ]]; then
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
