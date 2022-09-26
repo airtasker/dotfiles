a14f4856f161f04dca9eea24ee539b8300a3043a
Micah van der Kolk <micahvdk@gmail.com>
2022-09-26 11:21:39 +1000

    update install fonts

--- install_fonts.sh
+++ install_fonts.sh
@@ -5,6 +5,34 @@ function iterm_get() {
   /usr/libexec/PlistBuddy -c "Print :$1" ~/Library/Preferences/com.googlecode.iterm2.plist
 }
 
+if [[ "$(uname)" == Darwin && $TERM_PROGRAM == iTerm.app ]]; then
+  (( $+commands[curl] )) || return
+  [[ $TERM_PROGRAM_VERSION == [2-9]* ]] || return
+  if [[ -f ~/Library/Fonts ]]; then
+    [[ -d ~/Library/Fonts && -w ~/Library/Fonts ]] || return
+  else
+    [[ -d ~/Library && -w ~/Library ]] || return
+  fi
+  [[ -x /usr/libexec/PlistBuddy ]] || return
+  [[ -x /usr/bin/plutil ]] || return
+  [[ -x /usr/bin/defaults ]] || return
+  [[ -f ~/Library/Preferences/com.googlecode.iterm2.plist ]] || return
+  [[ -r ~/Library/Preferences/com.googlecode.iterm2.plist ]] || return
+  [[ -w ~/Library/Preferences/com.googlecode.iterm2.plist ]] || return
+  local guid1 && guid1="$(iterm_get '"Default Bookmark Guid"' 2>/dev/null)" || return
+  local guid2 && guid2="$(iterm_get '"New Bookmarks":0:"Guid"' 2>/dev/null)" || return
+  local font && font="$(iterm_get '"New Bookmarks":0:"Normal Font"' 2>/dev/null)" || return
+  [[ $guid1 == $guid2 ]] || return
+  [[ $font != 'MesloLGS-NF-Regular '<-> ]] || return
+  # [[ $font == (#b)*' '(<->) ]] || return
+  [[ $font == 'MesloLGSNer-Regular '<-> ]] && iterm2_old_font=1
+  iterm2_font_size=$match[1]
+  terminal=iTerm2
+  return 0
+fi
+return 1
+
+
 command mkdir -p -- ~/Library/Fonts || exit
 local style
 for style in Regular Bold Italic 'Bold Italic'; do
