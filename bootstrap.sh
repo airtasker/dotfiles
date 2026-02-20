038b247e75378f981e13f97d00f2d813e3ceccd9
Nam Nguyen <thanhnam.nguyen@airtasker.com>
2026-02-20 13:33:35 +1100

    Fix 'omz: command not found' during bootstrap
    Source Oh My Zsh before attempting to run omz update command.
    Previously, the omz command wasn't available in PATH during bootstrap
    even if Oh My Zsh was already installed, causing a harmless but
    confusing 'command not found' error.
    
    This fix sources oh-my-zsh.sh to load the omz command before using it.

--- bootstrap.sh
+++ bootstrap.sh
@@ -146,8 +146,10 @@ if [[ ! -d $HOME/.oh-my-zsh ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 else
     # Update Oh My Zsh if already installed
-    if [[ $(command -v omz) ]]; then
-    	omz update
+    # Source Oh My Zsh first to make omz command available
+    if [[ -f $HOME/.oh-my-zsh/oh-my-zsh.sh ]]; then
+        source $HOME/.oh-my-zsh/oh-my-zsh.sh
+        omz update
     fi
 fi
 
